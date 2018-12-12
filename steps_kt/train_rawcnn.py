c3ca1171d16b6678e243515d2cbf122e2f6dc3f6
D S Pavan Kumar <dspavankumar@gmail.com>
2018-12-12 17:27:44 +0100

    Updated license

--- steps_kt/train_rawcnn.py
+++ steps_kt/train_rawcnn.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
-##  Copyright (C) 2018 Idiap Research Institute
-##  Authors:
-##      S. Pavankumar Dubagunta
-##      Vinayak Abrol
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License, or
-##  (at your option) any later version.
-##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
-##
-##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## Copyright (c) 2018 Idiap Research Institute, http://www.idiap.ch/
+## Written by S. Pavankumar Dubagunta <pavankumar [dot] dubagunta [at] idiap [dot] ch>,
+## Vinayak Abrol and Mathew Magimai Doss <mathew [at] idiap [dot] ch>.
+## 
+## This file is part of LR-CNN.
+## 
+## LR-CNN is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License version 3 as
+## published by the Free Software Foundation.
+## 
+## LR-CNN is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with LR-CNN. If not, see <http://www.gnu.org/licenses/>.
 
 
 import keras
